495edd8bfae9dccd48b4bd1dc9f77ad4fcfeea54
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-02-29 17:20:12 +0200

    Started Stopify workshop

---
+++ Stopify/src/Stopify/Data/Stopify.Data.Models/StopifyUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Stopify.Data.Models
+{
+    public class StopifyUser : IdentityUser
+    {
+        public StopifyUser()
+        {
+
+        }
+
+        public string FullName { get; set; }
+    }
+}
---
+++ Stopify/src/Stopify/Data/Stopify.Data/StopifyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Stopify.Data.Models;
+
+namespace Stopify.Data
+{
+    public class StopifyDbContext : IdentityDbContext<StopifyUser, IdentityRole, string>
+    {
+        DbSet<Product> Products { get; set; }
+
+        public StopifyDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
